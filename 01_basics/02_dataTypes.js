fe99216bceac720680fe8e086d0f8b55ff6bb5bc
Tushar Vaghela <tusharvaghela5027@gmail.com>
2024-12-27 23:16:08 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code as the new version of the js code
+
+// alert("hello!"); // we are using  nodeJs, not browser
+
+let name = "tushar"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint 
+//String => ""
+//boolean => true/false
+//null => standalone value 
+//udefined => 
+//symbol => fro uniqueness
+//Objects
+
+console.log(typeof "tushar"); /*=> string */
+console.log(typeof age) /*=> number*/ 
+console.log(typeof null); /*=> object*/
+console.log(typeof undefined); /*=> undefined*/
