f2b8f9ae5fcc3a287f11266cab63ef705eab29ef
Tushar Vaghela <tusharvaghela5027@gmail.com>
2024-12-28 21:55:47 +0530

    datatype memory allocation

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -37,3 +37,38 @@ const myFunction = function(a, b){
 }
 // console.log(typeof myFunction)
 // myFunction(4,5);
+
+//******************************************************************************/
+
+// Stack (Primitive), Heap(Non- Primitive)
+
+let myYoutubeName = "tusharVgahela";
+
+let anotherName = myYoutubeName;
+
+// console.log(anotherName);
+
+anotherName = "vaghelaTushar"
+
+// console.log(anotherName);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne;
+
+// console.log(userOne);
+// console.log(userTwo);
+
+userTwo.email = "vaghela@google.com";
+
+// console.log(userOne);
+// console.log(userTwo);
+
+//-------------------------------------------------------------------------------------
+
+//NOTE:-
+//-> Heap memory use the reference for the memory allocation.
+//-> Stack make the copt of the variable in the stack memory.
