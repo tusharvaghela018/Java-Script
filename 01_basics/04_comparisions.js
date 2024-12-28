59d2b72f36f0b0c39537a0f23ea9cbfb00a04390
Tushar Vaghela <tusharvaghela5027@gmail.com>
2024-12-28 12:56:28 +0530

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1); // true
+// console.log(2 < 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// console.log( null < 0); // false
+// console.log( null > 0); // false
+// console.log( null == 0); // false
+// console.log( null >= 0); // true
+// console.log( null <= 0); // true
+// console.log(null == undefined); // true
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+// console.log(undefined >= 0); // false
+// console.log(undefined <= 0); // false
+// console.log(undefined == null); // true
+
+console.log("tushar" == "TUSHAR"); // false
+console.log("tushar" == "tushar"); // true
+console.log("tushar" === "TUSHAR"); // false
+console.log("33" == 33); // true
+console.log("33" === 33); // false (strict check)
